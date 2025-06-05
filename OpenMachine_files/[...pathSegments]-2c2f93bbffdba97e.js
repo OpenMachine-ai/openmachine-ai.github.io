6fa76e06d49964d2f3e2b633845f959099511900
Nils Graef <info@openmachine.ai>
2025-06-05 11:41:01 -0700

    adding website downloaded from soloist

---
+++ OpenMachine_files/[...pathSegments]-2c2f93bbffdba97e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[850],{9027:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[...pathSegments]",function(){return u(7340)}])}},function(n){n.O(0,[211,620,386,456,765,929,340,774,888,179],function(){return n(n.s=9027)}),_N_E=n.O()}]);
